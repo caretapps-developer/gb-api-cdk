dd656e8e23cf7a582ee1c4e6436bb0855da87ecf
amemon1 <azeem_memon@intuit.com>
2025-05-23 18:17:52 -0700

    Initial commit

---
+++ bin/gb2-api-cdk.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { Gb2ApiCdkStack } from '../lib/gb2-api-cdk-stack';
+
+const app = new cdk.App();
+new Gb2ApiCdkStack(app, 'Gb2ApiCdkStack', {
+  /* If you don't specify 'env', this stack will be environment-agnostic.
+   * Account/Region-dependent features and context lookups will not work,
+   * but a single synthesized template can be deployed anywhere. */
+
+  /* Uncomment the next line to specialize this stack for the AWS Account
+   * and Region that are implied by the current CLI configuration. */
+  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+
+  /* Uncomment the next line if you know exactly what Account and Region you
+   * want to deploy the stack to. */
+  // env: { account: '123456789012', region: 'us-east-1' },
+
+  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+});
